3c9ed4081026ae52502fb50c743d7912e6cb3c86
vivekjain52 <jainvivek52@gmail.com>
2016-11-18 15:30:05 +0530

    Initial project created and checked-in

---
+++ src/main/java/com/nuance/CdiNextApplication.java
@@ -0,0 +1,12 @@
+package com.nuance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdiNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdiNextApplication.class, args);
+	}
+}
